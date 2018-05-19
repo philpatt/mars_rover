629db5b1dbba62f11e37cdd68da5fc451e72728a
philpatt <phillip.s.patterson1@gmail.com>
2018-05-19 13:23:15 -0700

    create main function to gather input for processing

---
+++ Mars.py
@@ -0,0 +1,9 @@
+
+
+class MarsPlateau(object):
+    # This class creates the landing area plateau for the rover
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.taken = []
--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+from Mars import MarsPlateau
+
+
 def main():
-    print('hello world')
+    landing_area_input = input('Please enter size of the landing plateau, separated by a space: ').split(' ')
+    landing_area = MarsPlateau(int(landing_area_input[0]), int(landing_area_input[1]))
+
+    print('landing area',landing_area)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     main()
