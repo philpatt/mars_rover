d2a4edaf74ae983c070e89f88d0f01e891e2a22d
philpatt <phillip.s.patterson1@gmail.com>
2018-05-19 16:35:59 -0700

    added turn left/turn right methods for rover

--- Rover.py
+++ Rover.py
@@ -2,20 +2,30 @@
 directions = ("N", "E", "S", "W")
 
 class Rover(object):
-    def __init__(self, x, y, direction, mars):
+    def __init__(self, x, y, direction, landingArea):
         self.x = x
         self.y = y
         self.direction = direction        
-        self.Mars = mars
+        self.landingArea = landing_area
         self.initial = (self.x, self.y, self.direction)
 
-    def turnLeft():
-        # rover turn left
 
-    def turnRight():
+    def turnRight(self):
         # rover turn right
+        
+        if directions.index(self.direction) == 3:
+            return self.direction = directions[0]
+        else:
+            return self.directions = directions[directions.index(self.direction) + 1]
 
-    def moveForward():
+    def turnLeft(self):
+        # rover turn left
+                if directions.index(self.direction) == 0:
+            return self.direction = directions[3]
+        else:
+            return self.directions = directions[directions.index(self.direction) - 1]
+
+    def moveForward(self):
         #rover move forwards
 
     
--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 from Mars import MarsPlateau
-from Rover import Rover
+from Rover import Rover, directions
 
-def intial_rover_position(landingArea):
+def get_intial_rover_position(landingArea):
     # create a rover by inputing an initial position
-    rover_input = input("Please enter rover's starting position: ").split(' ')
-    rover = Rover(rover_input, landingArea)
+    rover = None
+    while not Rover:
+        rover_input = input("Please enter rover's starting position and direction: ").split(' ')
+        try:
+            rover = Rover(int(rover_input[0]), int(rover_input[1]), rover_input[2], landingArea)
+        Exception Error as err:
+            print(err)
+    return rover
 
 def land_rover(rover,landingArea):
     # input directions for the rover to land 
-    directions_input = input("Please enter a sequence of directions:\nOnly use 'L','R','M' ").split(' ').upper
+    commands_input = input("Please enter a sequence of commandss:\nOnly use 'L','R','M' ").split(' ').upper
 
-def move(instructions,rover):
+def move(commands,rover):
     # move the rover
-    for instruction in instructions:
-        # rover turn left
-        # rover turn right
-        # rover move forward
+    for command in commands:
+
+        if command == "L":
+            rover.turnLeft()
+        elif command = "R":
+            rover.turnRight()
+        else:
+            rover.moveForward()
+
         
         
 
@@ -29,7 +40,7 @@ def main():
     landing_area_input = input('Please enter size of the landing plateau, separated by a space: ').split(' ')
     landing_area = MarsPlateau(int(landing_area_input[0]), int(landing_area_input[1]))
 
-    rover = create_rover(landing_area) # Create rover
+    rover = get_intial_rover_position(landing_area) # Create rover
     land_rover(rover, landing_area) # Once rover is created => try to land it based on the given directions
 
 if __name__ == "__main__":
