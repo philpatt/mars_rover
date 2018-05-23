3ff64a770b9c2d2dac4ea339f12e7e5cd42a2ba7
philpatt <phillip.s.patterson1@gmail.com>
2018-05-22 22:28:34 -0700

    added test for rover.get_checked

--- RoverTests.py
+++ RoverTests.py
@@ -1,18 +1,29 @@
 import unittest
 from Rover import *
-from Mars import CreateMarsLandingArea
+from Mars import create_mars_landing_area
 
-class RoverTests(unittest.TestCase):
-    def testCreateRoverInitialPosition(self):
-        landingArea = CreateMarsLandingArea(5, 5)
-        rover = CreateRover(1, 2, "N", landingArea)
+class rover_tests(unittest.TestCase):
+    def test_create_rover_initial_position(self):
+        landing_area = create_mars_landing_area((5, 5))
+        rover = create_rover(1, 2, "N", landing_area)
+
         x = rover.x
         y = rover.y
         direction = rover.direction
-        RoverInitialPosition = x, y, direction
-        ExpectedRoverInitialPosition = 1, 2, "N"
-        
-        self.assertEqual(RoverInitialPosition, ExpectedRoverInitialPosition)
+        rover_initial_position = x, y, direction
+        expected_rover_initial_position = 1, 2, "N"
+
+        self.assertEqual(rover_initial_position, expected_rover_initial_position)
+
+    def test_check_move(self):
+        landing_area = create_mars_landing_area((5, 5))
+        rover = create_rover(1, 2, "N", landing_area)
+        self.assertEqual(rover.check_move(rover.x, rover.y, landing_area), True)
+
+
+
+
+
 
 
 if __name__ == '__main__':
