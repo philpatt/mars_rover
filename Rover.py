80ffff901f9cbfcd258c9d69a751a944fed80a40
philpatt <phillip.s.patterson1@gmail.com>
2018-05-22 22:16:00 -0700

    adding more comments to program

--- Rover.py
+++ Rover.py
@@ -11,17 +11,15 @@ class create_rover(object):
         self.initial = (self.x, self.y, self.direction)
 
     def get_current_position(self):
-        # This function allow the rover to access its current position at any point after it is instantiated.
         return [self.x, self.y]
 
     def rover_to_intial_position(self):
-        # When this function is called the rover will be returned to its intial position
         self.x = self.initial[0]
         self.y = self.initial[1]
         self.direction = self.initial[2]
 
     def x_position_check(self, landing_area):
-        # This function is used during navigation. After the Rover uses navigate to turn left, turn right, and move forward, this checks the x value to make sure it is within the landing area dimensions.
+        # is X within landing areay dimensions
         if (self.x < 0 or self.x > landing_area.x):
             self.rover_to_intial_position()
             raise ValueError('\nRover out of landing area, and lost in space!\n Try another Rover!')
@@ -29,8 +27,7 @@ class create_rover(object):
             return True
 
     def y_position_check(self, landing_area):
-        # This function is used during navigation. After the Rover uses navigate to turn left, turn right, and move forward, this checks the y value to make sure it is within the landing area dimensions.
-
+        # is y within landing areay dimensions
         if (self.y < 0 or self.y > landing_area.y):
             self.rover_to_intial_position()
             raise ValueError('\nRover out of landing area, and lost in space!\n Try another Rover!')
@@ -38,8 +35,7 @@ class create_rover(object):
             return True
 
     def direction_position_check(self):
-        # This function is used during navigation. After the Rover uses navigate to turn left, turn right, and move forward, this checks the directional value to make sure it is a direciton that the Compass .
-
+        # is direction part of compass list
         if self.direction.upper() not in compass:
             self.rover_to_intial_position()
             raise ValueError('Error: Incorrect direction value, going back to initial position.\n Try again!')
@@ -47,28 +43,19 @@ class create_rover(object):
             return True
 
     def turn_right(self):
-        # If the Rover's direction matches the compass' direction at position 3 ('W' - West), then change the Rover's direction to "N"-North(compass[0]).
-        # If the Rover's direction is not 'W'-West, then change the Rover's Direction to the compass direction that is associated with the Rover's direction and increase by 1.
-
         if compass.index(self.direction) >= 3:
             self.direction = compass[0]
         else:
             self.direction = compass[compass.index(self.direction) + 1]
 
-
     def turn_left(self):
-        # if the Rovers direction matches the compasses direction at position 0 ('N' - North), then change the Rover's direction to "W"-West(compass[3]).
-        # if the Rover's direction is not 'N'-North, then change the Rover's Direction to the compass direction that is associated with the Rover's direction and decrease by 1.
-
         if compass.index(self.direction) <= 0:
             self.direction = compass[3]
         else:
             self.direction = compass[compass.index(self.direction) - 1]
 
     def move_forward(self, landing_area):
-        # Move Forward and check to see if that is space is available.
         new_position = self.get_current_position()
-
         if self.direction == "N":
             new_position[1] += 1
         elif self.direction == "S":
@@ -79,7 +66,6 @@ class create_rover(object):
             new_position[0] -= 1
         else:
             raise ValueError('ERROR: Incorrect directional value, Rover sent back to initial position')
-
         # Check if out of bounds or occupied by other rovers
         if self.check_move(new_position[0], new_position[1], landing_area):
             self.x = new_position[0]
@@ -89,6 +75,7 @@ class create_rover(object):
             raise ValueError
 
     def check_move(self, position_x, position_y, landing_area):
+        # if out of bounds or crash in to other rover, return to rovers original spot
         if position_x < 0 or position_y < 0 or position_x >= landing_area.x or position_y >= landing_area.y:
             print('\n---------------------------')
             print('Almost drove out of bounds!\n Rover return to intial position!')
--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from Rover import create_rover, compass, navigation_commands
 import sys
 
 def main():
-    #
     print('===============================')
     print('~~ MARS ROVER TECH CHALLENGE ~~')
     print('===============================')
@@ -20,37 +19,24 @@ def main():
 
     #2 - Acquire Rovers to land
     acquiring_rovers = True
-
     while acquiring_rovers:
         # Instantiate Rover
         rover = get_intial_rover_position_input(landing_area)
         # Attempt to land Rover on Landing Area
         land_rover(rover, landing_area)
 
-
-
 def get_landing_area():
-    #
     landing_area_dimensions_prompt = '\n--------------\n** Please enter two numbers, separated by a space, to determine the dimensions for the Mars landing Area:\n--------------\n => '
-
     is_create_landing_area_running = True
-
     while (is_create_landing_area_running):
-
         landing_area_input = input(landing_area_dimensions_prompt).split(' ')
-
         if is_landing_area_input_valid(landing_area_input):
-
             landing_area = create_mars_landing_area(landing_area_input)
-
             is_create_landing_area_loop_running = False
-
             return landing_area
 
 
 def is_landing_area_input_valid(user_input):
-
-
     if len(user_input) == 2 and user_input[0].isdigit() and user_input[1].isdigit():
         x = int(user_input[0])
         y = int(user_input[0])
@@ -80,7 +66,7 @@ def is_landing_area_input_valid(user_input):
 def get_intial_rover_position_input(landing_area):
     # Here is where we create a Rover Object attached to the Landing Area Object
     # 1. make sure input is intergers
-    # 2. Make sure rover in bounds
+    # 2. Make sure rover inbounds
     # 3. Make sure rover not in landing are taken array
     # We also check too see if the input is 'report' or 'exit'
 
@@ -98,8 +84,6 @@ def get_intial_rover_position_input(landing_area):
                 return rover
         except ValueError:
             print('Error 1')
-
-
 
 def is_rover_input_valid(user_input):
 
@@ -131,7 +115,6 @@ def is_rover_position_taken(user_input,landing_area):
 def is_rover_inbounds(coordinates, boundary):
     x = int(coordinates[0])
     y = int(coordinates[1])
-
     if ( x < 0 or x > boundary.x):
         print('\n--------------------------------------------')
         print('ERROR: X Coordinate out of range! Try again!')
@@ -144,8 +127,6 @@ def is_rover_inbounds(coordinates, boundary):
         return False
     return True
 
-
-
 def user_input_check(user_input, landing_area):
     if 'end'.upper() in user_input.upper():
         print('========================')
@@ -156,10 +137,13 @@ def user_input_check(user_input, landing_area):
         show_rover_report(landing_area)
     return user_input
 
-
 def land_rover(rover,landing_area):
     # Using only a sequence of L, M ,R. The user gives commands for the Rover to take in and see if it can land or not.
-
+    # 1. Make sure Command sequence is valid (only LMR)
+    # 2. Make sure rover command sequence allows rover to stay inbounds
+    # 3. Make sure rover command sequence doesnt move rover into an occupied spot
+    # 4. Check too see if the input is 'report' or 'exit'
+    # Return Rover back to initial position if above criteria is not met
     moved = False
     command_sequence_prompt = "\n--------------\nPlease enter a sequence of commands:\nOnly use 'L','R','M' or 'end' to terminate\n--------------\n=> "
     while not moved:
@@ -167,23 +151,17 @@ def land_rover(rover,landing_area):
             commands = user_input_check(input(command_sequence_prompt), landing_area).upper()
             if 'REPORT' not in commands and is_command_sequence_valid(commands):
                 navigate(commands, rover, landing_area)
-                # print('navigating...')
-                # If commands are determined valid and the rover object is updated, another check comes to determine
             else:
                 raise ValueError
-
-            if (rover.y_position_check(landing_area) and rover.x_position_check(landing_area) and rover.direction_position_check()):# Here is where the rover checks if the landing coordinates are acceptable
+            if (rover.y_position_check(landing_area) and rover.x_position_check(landing_area) and rover.direction_position_check()):
                 landing_area.taken.append([rover.x, rover.y, rover.direction])
                 moved = True
         except ValueError:
             print('Rover back to', rover.initial)
-
-
     if moved:
         print('\n-----------------------------')
         print('~~~~~~ Rover Landed ! ~~~~~~')
         print('----------------------------')
-
         is_landing_area_full(landing_area)
         show_rover_report(landing_area)
         return
@@ -194,7 +172,6 @@ def is_command_sequence_valid(commands):
         print('No command sequence entered. Try Again!')
         print('\n=====================================')
         return False
-
     for command in commands:
         if command not in navigation_commands:
             print('\n----------------------------------------')
@@ -203,33 +180,28 @@ def is_command_sequence_valid(commands):
             print("Rover Crashed! Attempt New Rover!")
             print('----------------------------------------')
             return False
-
     return True
 
 def navigate(commands, rover, landing_area):
-
     # After a 'Move Forward' request is made, the rover checks to see if that space is occupied.
     # If space is NOT occupied, then the rover Object is updated and moves on in the landRover() process.
     for command in commands:
         if command == navigation_commands[0]:
-            rover.turn_left() # => see Rover.py for more details
+            rover.turn_left()
         elif command == navigation_commands[1]:
-            rover.turn_right() # => see Rover.py for more details
+            rover.turn_right()
         elif command == navigation_commands[2]:
-            rover.move_forward(landing_area) # => see Rover.py for more details
+            rover.move_forward(landing_area)
         else:
             return False
     return True
 
-
 def show_rover_report(landing_area):
-    # The Rover Report prints the taken Landing Area list, it also is shown after each succesful landing
     print('\n-----------------------')
     print('*~* Rover Report *~*')
     print('-----------------------')
     print('Landing Area: ' + str(landing_area.x) + ' x ' + str(landing_area.y))
     print ("Landed Rovers:")
-
     for rover in landing_area.taken:
         print("{} {} {}".format(rover[0],rover[1],rover[2]))
     print('-----------------------')
